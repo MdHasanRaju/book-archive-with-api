ba9c67604a63710802395d1cd900b62e348e0251
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-09-02 11:39:24 +0600

    button color added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const loadSearchedData = books => {
             <img height="300px" src="https://covers.openlibrary.org/b/id/${
               book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Name: ${book.title}</h5>
+                <h5 class="card-title">Title: ${book.title}</h5>
                 <p class="card-title">Author: ${book.author_name}</p>
                 <p class="card-text">Publisher: ${book.publisher}, ${book.first_publish_year ? book.first_publish_year :""}</p>
             </div>
