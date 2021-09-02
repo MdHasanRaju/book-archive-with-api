7f4574e0249afa34389bb71628186228a0faaead
MdHasanRaju <mdhasanraju2017@gmail.com>
2021-09-02 17:52:01 +0600

    eror case added

--- app.js
+++ app.js
@@ -1,36 +1,84 @@
 const searchResult = () => {
-    const searchField = document.getElementById('search-field')
-    const searchText = searchField.value;
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-    fetch(url)
-    .then(res => res.json())
-    .then(data => loadSearchedData(data.docs))
-}
+  /* search-field */
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
 
-const loadSearchedData = books => {
-    // console.log(books)
+  console.log(searchText);
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-    const booksContainer = document.getElementById('books');
-    books.forEach(book => {
-        console.log(book.cover_i)
-        const div = document.createElement('div');
-        div.innerHTML = `
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => loadSearchedData(data.docs));
+};
+
+const loadSearchedData = (books) => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(books);
+
+  // all warning case
+  const msg = [
+    "No result found",
+    "Total result found:",
+    "Search field must not be empty",
+  ];
+
+  const result =
+    searchText === ""
+      ? msg[2]
+      : books.length > 0
+      ? msg[1] + " " + books.length
+      : msg[0];
+
+  const validationMsg = document.getElementById("validationMsg");
+  validationMsg.innerText = result;
+
+  /* all searched items */
+  const booksContainer = document.getElementById("books");
+  booksContainer.textContent = "";
+  books.forEach((book) => {
+    if (book.cover_i) {
+      const div = document.createElement("div");
+      div.innerHTML = `
         <div class="col">
           <div class="card h-100">
-            <img height="300px" src="https://covers.openlibrary.org/b/id/${
-              book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img height="250px" src="https://covers.openlibrary.org/b/id/${
+              book.cover_i
+            }-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Title: ${book.title}</h5>
                 <p class="card-title">Author: ${book.author_name}</p>
-                <p class="card-text">Publisher: ${book.publisher}, ${book.first_publish_year ? book.first_publish_year :""}</p>
+                <p class="card-text">Publisher: ${book.publisher}</p>
+                <P>Release date: ${
+                  book.first_publish_year ? book.first_publish_year : ""
+                }</p>
             </div>
           </div>
         </div>
         `;
-        booksContainer.appendChild(div);
-    })
-}
-
-// ${book.cover_i}
-// $book.author_name.cover_i
+      booksContainer.appendChild(div);
+    }else{
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class="col">
+          <div class="card h-100">
+            <img height="250px" src="https://covers.openlibrary.org/b/id/10909258-M.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Title: ${book.title}</h5>
+                <p class="card-title">Author:  ${
+                  book.author_name ? book.author_name : ""
+                }</p>
+                <p class="card-text">Publisher: ${book.publisher}</p>
+                <P>Release date: ${
+                  book.first_publish_year ? book.first_publish_year : ""
+                }</p>
+            </div>
+          </div>
+        </div>
+        `;
+      booksContainer.appendChild(div);
+    }
+    
+  });
+};
